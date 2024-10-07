c694e434430fa63b44bba13cdcb0ea5b451133df
MarcusGustavsson <marcus.gustavsson@iths.se>
2024-10-07 20:44:40 +0200

    Initial commit

---
+++ src/main/java/org/example/warehouse/Category.java
@@ -0,0 +1,10 @@
+package org.example.warehouse;
+
+public class Category {
+
+    public static Object of(String test) {
+    }
+
+    public boolean getName() {
+    }
+}
---
+++ src/main/java/org/example/warehouse/Warehouse.java
@@ -0,0 +1,14 @@
+package org.example.warehouse;
+
+public class Warehouse {
+    public static Warehouse getInstance(String myStore) {
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean getProducts() {
+        return false;
+    }
+}
