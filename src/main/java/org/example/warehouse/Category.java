3cb9b1c8d3c4a1ec5d26cb065b77a2d9c26057f3
MarcusGustavsson <marcus.gustavsson@iths.se>
2024-10-10 11:58:57 +0200

    Added toUpperCase, instances return

--- src/main/java/org/example/warehouse/Category.java
+++ src/main/java/org/example/warehouse/Category.java
@@ -6,11 +6,7 @@ import java.util.Map;
 public class Category {
     private static final Map<String, Category> instances = new HashMap<>();
     private final String name;
-    /*Ha en of-metod som skapar och returnerar en instans.
-    Se till att namnet alltid börjar med en versal.
-    Använda en singleton-struktur, där samma kategori med samma namn alltid returnerar samma instans.
-    X Kasta ett IllegalArgumentException om namnet är null.
-    X Inte ha någon publik konstruktor.*/
+
     private Category() {
         this.name = name.substring(0, 1).toUpperCase() + name.substring(1);
     }
