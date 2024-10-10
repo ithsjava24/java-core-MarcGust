bab23e19eb657e9fae64474a5f4b79029b8a220d
MarcusGustavsson <marcus.gustavsson@iths.se>
2024-10-10 11:57:07 +0200

    Added private constructor, exception and HashMap

--- src/main/java/org/example/warehouse/Category.java
+++ src/main/java/org/example/warehouse/Category.java
@@ -1,19 +1,25 @@
 package org.example.warehouse;
 
-public class Category {
+import java.util.HashMap;
+import java.util.Map;
 
-    private String name;
+public class Category {
+    private static final Map<String, Category> instances = new HashMap<>();
+    private final String name;
     /*Ha en of-metod som skapar och returnerar en instans.
     Se till att namnet alltid börjar med en versal.
     Använda en singleton-struktur, där samma kategori med samma namn alltid returnerar samma instans.
-    Kasta ett IllegalArgumentException om namnet är null.
-    Inte ha någon publik konstruktor.*/
+    X Kasta ett IllegalArgumentException om namnet är null.
+    X Inte ha någon publik konstruktor.*/
     private Category() {
-
+        this.name = name.substring(0, 1).toUpperCase() + name.substring(1);
     }
 
-    public static Category of(String dairy) {
-        return null;
+    public static Category of(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Name cannot be null");
+        }
+        return instances.computeIfAbsent(name.toLowerCase(), Category::new);
     }
 
     public String getName() {
