b2ceb6ad017ff0b1d61d0aa65600874f0dfbf8e1
MarcusGustavsson <marcus.gustavsson@iths.se>
2024-10-11 12:12:02 +0200

    refractoring and optimizing

--- src/main/java/org/example/warehouse/Category.java
+++ src/main/java/org/example/warehouse/Category.java
@@ -36,8 +36,7 @@ public class Category {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Category)) return false;
-        Category category = (Category) o;
+        if (!(o instanceof Category category)) return false;
         return name.equals(category.name);
     }
 
