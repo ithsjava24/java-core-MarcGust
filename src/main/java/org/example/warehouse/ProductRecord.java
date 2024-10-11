a875fa9ec2225d74b45cbc5cd5dafecc2e9ae12d
MarcusGustavsson <marcus.gustavsson@iths.se>
2024-10-11 16:33:20 +0200

    refractoring code

--- src/main/java/org/example/warehouse/ProductRecord.java
+++ src/main/java/org/example/warehouse/ProductRecord.java
@@ -1,15 +1,15 @@
-package org.example.warehouse;
+    package org.example.warehouse;
 
-import java.math.BigDecimal;
-import java.util.UUID;
+    import java.math.BigDecimal;
+    import java.util.UUID;
 
-public record ProductRecord(UUID id, String name, Category category, BigDecimal price) {
+    public record ProductRecord(UUID id, String name, Category category, BigDecimal price) {
 
-    public UUID uuid() {
-        return id; // Returnerar id som UUID
+        public UUID uuid() {
+            return id; // Returnerar id som UUID
+        }
+
+        public ProductRecord withPrice(BigDecimal newPrice) {
+            return new ProductRecord(this.id, this.name, this.category, newPrice);
+        }
     }
-
-    public ProductRecord withPrice(BigDecimal newPrice) {
-        return new ProductRecord(this.id, this.name, this.category, newPrice);
-    }
-}
